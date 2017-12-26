0b2057612a06ef51406a77eaa86b2a94286ffe10
Rohit <rohitsa40@gmail.com>
2017-12-26 10:35:40 +0530

    Updated for graphics ubuntu

--- RAWbresenhams line.cpp
+++ RAWbresenhams line.cpp
@@ -11,12 +11,17 @@ using namespace std;
 // function for line generation
 void bresenham(int x1, int y1, int x2, int y2)
 {
+  //To enable graphics for ubuntu
+   int gd = DETECT,gm=VGAMAX;
+   initgraph(&gd,&gm,NULL);
+ 
    int m_new = 2 * (y2 - y1);
    int slope_error_new = m_new - (x2 - x1);
    for (int x = x1, y = y1; x <= x2; x++)
    {
-      cout << "(" << x << "," << y << ")\n";
- 
+     //plot the pixel 
+      putpixel(x,y,RED);
+    
       // Add slope to increment angle formed
       slope_error_new += m_new;
  
@@ -28,6 +33,7 @@ void bresenham(int x1, int y1, int x2, int y2)
          slope_error_new  -= 2 * (x2 - x1);
       }
    }
+   closegraph();
 }
  
 // driver function
